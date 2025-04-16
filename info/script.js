31aa2efda270fdffd2fe483cf42834ecee9e3a8c
Dan4ikk <da8493137@gmail.com>
2025-04-17 00:21:39 +0300

    Add files via upload

---
+++ info/script.js
@@ -0,0 +1,6 @@
+// Можно добавить дополнительные эффекты или логику
+// Например, анимацию при загрузке страницы
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Сайт Moonline загружен!');
+});
