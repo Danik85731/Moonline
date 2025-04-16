b976642e7b816d0b08af2fe2340b4a4029a90460
Dan4ikk <da8493137@gmail.com>
2025-04-17 00:21:54 +0300

    Delete info directory

--- info/script.js
+++
@@ -1,6 +0,0 @@
-// Можно добавить дополнительные эффекты или логику
-// Например, анимацию при загрузке страницы
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Сайт Moonline загружен!');
-});
